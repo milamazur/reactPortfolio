5c3e8486a593702ead108dedbb7c50aab8a94f70
milamazur <r0951867@student.thomasmore.be>
2024-06-08 16:37:51 +0200

    no flags

--- src/pages/about/languages.tsx
+++ src/pages/about/languages.tsx
@@ -1,5 +1,4 @@
 import {FunctionComponent} from 'react'
-import Flag from 'react-world-flags'
 
 const Languages: FunctionComponent = () => {
     return (
@@ -7,23 +6,18 @@ const Languages: FunctionComponent = () => {
             <ul className=" place-items-center ml-14">
                 <li className="grid grid-cols-2 w-40 items-end text-left mb-6 ">
                     <p className="text-lg">English</p>
-                    <Flag className="w-11 " code="gb"/>
                 </li>
                 <li className="grid grid-cols-2 w-40 items-end text-left mb-6 ">
                     <p className="text-lg">Polish</p>
-                    <Flag className="w-11" code="pl"/>
                 </li>
                 <li className="grid grid-cols-2 w-40 items-end text-left mb-6">
                     <p className="text-lg">Dutch</p>
-                    <Flag className="w-11" code="be"/>
                 </li>
                 <li className="grid grid-cols-2 w-40 items-end text-left mb-6">
                     <p className="text-lg">Russian</p>
-                    <Flag className="w-11" code="ru"/>
                 </li>
                 <li className="grid grid-cols-2 w-40 items-end text-left mb-6">
                     <p className="text-lg">Spanish</p>
-                    <Flag className="w-11" code="es"/>
                 </li>
             </ul>
         </div>
