e2cc01b497ac2fc9e6a8442d92702d7ed8c76110
milamazur <r0951867@student.thomasmore.be>
2024-06-08 18:01:55 +0200

    noImages

--- src/pages/portfolio/images.tsx
+++ src/pages/portfolio/images.tsx
@@ -1,11 +1,11 @@
 import {FunctionComponent} from 'react'
 import {IImages} from '../../models/IImages.ts'
-const Images: FunctionComponent<IImages> = ({ pictures}) => {
+const Images: FunctionComponent<IImages> = () => {
     return (
         <div className="flex flex-wrap justify-center">
-            {pictures.map(p =>
+            {/* {pictures.map(p =>
                 <img key={p.id} src={p.imgSrc} alt="pictures" className="w-full sm:w-1/2 lg:w-1/3 m-4 max-w-md " />
-            )}
+            )} */}
         </div>
     )
 }
